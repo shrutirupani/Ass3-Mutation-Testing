7347ebf911878d3aef2b6bd7d9378a0b873b7a4d
Shruti Rupani <srupani8595@conestogac.on.ca>
2019-07-01 16:13:20 -0400

    Add project files.

---
+++ AwesomeCalculatorTests/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AwesomeCalculator;
+using NUnit.Framework;
+
+
+namespace CalcAppTest
+
+{
+    [TestFixture]
+    class CalcTests
+    {
+        [Test]
+        public void GetAddition_Input3point4and5point6_Returns9point0()
+        {
+
+            //Arrange
+            double number1 = 3.4;
+            double number2 = 5.6;
+
+            double expectedResult = number1 + number2;
+
+            Calc testCalc = new Calc(number1, number2);
+
+            //Act
+            double actualResult = testCalc.GetAddition();
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        
+    }
+}
